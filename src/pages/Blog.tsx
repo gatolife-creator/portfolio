560a6928876ae043f5e707e31ae1d09ca3441505
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-03-24 17:34:47 +0900

    各ページに拡大アニメーションを実装。

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,12 +1,22 @@
+import { motion } from "framer-motion";
+
+import Background from "../components/Background";
 import Main from "../components/Main";
 import TransitionAnimation from "../components/TransitionAnimation";
 
 const Blog = () => {
   return (
     <TransitionAnimation>
+      <Background />
       <Main>
-        <h3 className="font-bold text-4xl text-center mt-[50px]">BLOG</h3>
-        <center className="mt-10">Coming Soon...</center>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ stiffness: 260, damping: 20, duration: 0.6 }}
+        >
+          <h3 className="font-bold text-4xl text-center mt-[50px]">BLOG</h3>
+          <center className="mt-10">Coming Soon...</center>
+        </motion.div>
       </Main>
     </TransitionAnimation>
   );
--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+import Background from "../components/Background";
 import Card from "../components/Card";
 import Main from "../components/Main";
 import TransitionAnimation from "../components/TransitionAnimation";
@@ -9,31 +11,38 @@ import SoundVisualization from "../images/sound-visualization.png";
 const Works = () => {
   return (
     <TransitionAnimation>
+      <Background />
       <Main>
-        <h3 className="font-bold text-4xl text-center mt-[50px]">WORKS</h3>
-        <div className="grid grid-cols-3 gap-4 mt-20">
-          <Card
-            src={BlockchainWorldImg}
-            title="Blockchain World"
-            description="A beginner-friendly introduction to blockchain. Received the MIC Award at the 25th National Jr/Sr HS Web Contest."
-            link="https://contest.japias.jp/tqj25/250150K/"
-            className="mx-auto"
-          />
-          <Card
-            src={SoundVisualization}
-            title="Sound Visualizer"
-            description="A simple sound visualizer using p5.js."
-            link="https://gatolife-creator.github.io/sound_visualization/"
-            className="mx-auto"
-          />
-          <Card
-            src={DeepSeaImg}
-            title="Deep Sea"
-            description="Invites you on a journey to the deep sea using p5.js."
-            link="https://gatolife-creator.github.io/deep-sea/"
-            className="mx-auto"
-          />
-        </div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ stiffness: 260, damping: 20, duration: 0.6 }}
+        >
+          <h3 className="font-bold text-4xl text-center mt-[50px]">WORKS</h3>
+          <div className="grid grid-cols-3 gap-4 mt-20">
+            <Card
+              src={BlockchainWorldImg}
+              title="Blockchain World"
+              description="A beginner-friendly introduction to blockchain. Received the MIC Award at the 25th National Jr/Sr HS Web Contest."
+              link="https://contest.japias.jp/tqj25/250150K/"
+              className="mx-auto"
+            />
+            <Card
+              src={SoundVisualization}
+              title="Sound Visualizer"
+              description="A simple sound visualizer using p5.js."
+              link="https://gatolife-creator.github.io/sound_visualization/"
+              className="mx-auto"
+            />
+            <Card
+              src={DeepSeaImg}
+              title="Deep Sea"
+              description="Invites you on a journey to the deep sea using p5.js."
+              link="https://gatolife-creator.github.io/deep-sea/"
+              className="mx-auto"
+            />
+          </div>
+        </motion.div>
       </Main>
     </TransitionAnimation>
   );
